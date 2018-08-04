21119ee4d5e25457617c209d0bbe652060019755
Elena Lapitskaya <e.a.lapitskaya@gmail.com>
2018-08-04 11:50:09 +0300

    Make the page min-height equal viewport height.

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,22 @@
-/**
- * Created by Leka on 28.07.2018.
- */
+'use strict';
+
+$(document).ready(function () {
+    calcBodyPaddingBottom();
+
+    $(window).resize(function () {
+        calcBodyPaddingBottom();
+    });
+
+
+    //set 'padding-bottom' for body, so absolute positioned footer doesn't cover page content
+    var footerHeight, bodyPaddingBottom;
+
+    function calcBodyPaddingBottom() {
+        footerHeight = $('.footer').outerHeight();
+        bodyPaddingBottom = footerHeight + 20;
+        $('body').css('padding-bottom', bodyPaddingBottom + 'px');
+    }
+
+    //for test, to see the page without content
+    $('.article').hide();
+});
