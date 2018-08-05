7cfaaf7e9249b1e764b5071e7897edc8b98e6595
Elena Lapitskaya <e.a.lapitskaya@gmail.com>
2018-08-05 23:29:18 +0300

    Realize article selection by request-btn press.

--- src/js/script.js
+++ src/js/script.js
@@ -59,10 +59,39 @@ $(document).ready(function () {
             filterByTags(this);
         } else {
             $('.article').show();
+            $('.output__no-result').hide();
             $('.js-f-tag-active').each(function () {
                 filterByTags(this);
             });
         }
+    });
+
+    //article selection by request-btn press
+    var btnCounter = 0;
+    $('.article__request-btn').on('click', function () {
+        if ($(this).hasClass('article__request-btn--active')) {
+            $(this).html('Pyydä tarjous!');
+            btnCounter -= 1;
+            if (btnCounter == 0) {
+                $('.header__request-btn-num').html('');
+            }
+            $(this).toggleClass('article__request-btn--active');
+        } else if (btnCounter < 3) {
+            $(this).html('✔ Lisätty listalle');
+            btnCounter += 1;
+            $(this).toggleClass('article__request-btn--active');
+        }
+        if ($('.article__request-btn').is('.article__request-btn--active')) {
+            $('.header__request-btn').addClass('header__request-btn--active');
+            var btnAddNum = ' (' + btnCounter + ')';
+            if(window.matchMedia('(min-width: 700px)').matches) {
+                btnAddNum = btnCounter;
+            }
+            $('.header__request-btn-num').html('').append(btnAddNum);
+        } else {
+            $('.header__request-btn').removeClass('header__request-btn--active');
+        }
+        return false;
     });
 
 });
